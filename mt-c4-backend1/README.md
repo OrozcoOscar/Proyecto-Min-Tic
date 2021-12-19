d00016c01f417ba698de381afa6bb3df7662fe3e
Oscar <oorozcop@unicartagena.edu.co>
2021-12-19 18:43:57 -0500

    Version mas estable

--- mt-c4-backend1/README.md
+++
@@ -1,6 +0,0 @@
-# Backend del proyecto de desarrollo web
-Aplicación para manejar el backend del proyecto
-###
-Este es un cambio directo en el README
-###
-Nueva entrada en el código
--- mt-c4-backend1/src/database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const connect = () => mongoose.connect("mongodb+srv://Noxiofull:a123@cluster0.jmfiy.mongodb.net/ciclo4-db?retryWrites=true&w=majority", {
-  autoIndex: true,
-})
-  .then(() => console.log('Connected to DB'))
-  .catch(err => console.error(err));
-
-export default connect;
